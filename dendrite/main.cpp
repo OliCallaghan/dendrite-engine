dc483d7f9003263e6a7439377495476939eaf435
OliCallaghan <OliCallaghan@icloud.com>
2017-10-26 17:47:51 +0100

    Initial Commit

---
+++ dendrite/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dendrite
+//
+//  Created by Oli Callaghan on 26/10/2017.
+//  Copyright © 2017 Oli Callaghan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
